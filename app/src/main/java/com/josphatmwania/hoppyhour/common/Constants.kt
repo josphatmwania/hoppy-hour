ea3e112d03210f34606dad5e52451418934d305a
josphatmwania <josphatmwania237@gmail.com>
2023-01-21 22:11:40 +0300

    added SettingsDatastore.kt

---
+++ app/src/main/java/com/josphatmwania/hoppyhour/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.josphatmwania.hoppyhour.common
+
+object Constants {
+    const val SHOW_ONBOARD_PREFERENCES = "show_onboard_preferences"
+    const val SHOW_ONBOARDING = "show_onboarding"
+}
---
+++ app/src/main/java/com/josphatmwania/hoppyhour/common/SettingsDatastore.kt
@@ -0,0 +1,39 @@
+package com.josphatmwania.hoppyhour.common
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+
+private val Context.datastore by preferencesDataStore(
+    name = Constants.SHOW_ONBOARD_PREFERENCES
+)
+class SettingsDatastore(private val context: Context) {
+    companion object {
+        val SHOW_ONBOARDING = booleanPreferencesKey(Constants.SHOW_ONBOARDING)
+    }
+
+    suspend fun updateOnboadingState(showOnboarding: Boolean, context: Context) {
+        context.datastore.edit { preferences ->
+            preferences[SHOW_ONBOARDING] = showOnboarding
+        }
+    }
+
+    val preferenceFlow = context.datastore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            preferences[SHOW_ONBOARDING] ?: true
+        }
+}
