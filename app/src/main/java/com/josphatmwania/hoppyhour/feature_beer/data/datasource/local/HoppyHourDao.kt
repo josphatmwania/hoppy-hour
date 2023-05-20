f9574bb85035fd536199d636fb659051dd201a3a
josphatmwania <josphatmwania237@gmail.com>
2023-05-20 10:35:40 +0300

    Add Insert fun

--- app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDao.kt
+++ app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDao.kt
@@ -1,8 +1,17 @@
 package com.josphatmwania.hoppyhour.feature_beer.data.datasource.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.josphatmwania.hoppyhour.feature_beer.domain.model.Beer
+
 
 @Dao
 interface HoppyHourDao {
+
+
+
+    @Insert
+    suspend fun insert(beer: Beer)
 
 }
--- app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/presentation/events/BeerEvent.kt
+++ app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/presentation/events/BeerEvent.kt
@@ -5,5 +5,6 @@ import com.josphatmwania.hoppyhour.feature_beer.domain.model.Beer
 data class BeerEvent(
     var data: Beer? = null,
     var isLoading: Boolean = false,
-    var message: String = ""
+    var message: String = "",
+
 )
