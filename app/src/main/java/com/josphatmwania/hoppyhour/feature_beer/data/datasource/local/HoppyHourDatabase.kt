41507c552457466d94e938d1139fc2277b9b36a7
josphatmwania <josphatmwania237@gmail.com>
2023-05-20 11:26:18 +0300

    Add a companion object Instance to keep a single instance of the DB

--- app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDatabase.kt
+++ app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDatabase.kt
@@ -1,10 +1,13 @@
 package com.josphatmwania.hoppyhour.feature_beer.data.datasource.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.josphatmwania.hoppyhour.common.Constants
 import com.josphatmwania.hoppyhour.feature_beer.domain.model.Amount
 import com.josphatmwania.hoppyhour.feature_beer.domain.model.BeerEntity
+import okhttp3.internal.concurrent.TaskRunner.Companion.INSTANCE
 
 @Database(
     entities = [BeerEntity::class],
@@ -15,11 +18,30 @@ abstract class HoppyHourDatabase : RoomDatabase() {
     abstract fun dao(): HoppyHourDao
 
     /**
-     * allows access to the methods to create or
+     * This Object allows access to the methods to create or
      * get the database and uses the class name as the qualifier.
+     * variable keeps a reference to the database, when one has been created.
+     * This helps maintain a single instance of the database opened at a given time
      */
 
-    companion object{
-        private val Instance: HoppyHourDatabase? = null
+
+    companion object {
+        @Volatile
+        private var Instance: HoppyHourDatabase? = null
+
+        fun getDatabase(context: Context): HoppyHourDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, HoppyHourDatabase::class.java, "Hoppy_Hour_database")
+                    .build()
+                    .also { Instance = it }
+            }
+
+        }
     }
+
+
+
+
+
+
 }
