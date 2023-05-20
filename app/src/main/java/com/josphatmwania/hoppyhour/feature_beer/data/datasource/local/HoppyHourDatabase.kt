3ecd320cb109a039725cf92f1c9c361af1196512
josphatmwania <josphatmwania237@gmail.com>
2023-05-20 11:05:54 +0300

    Add a companion object Instance

--- app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDatabase.kt
+++ app/src/main/java/com/josphatmwania/hoppyhour/feature_beer/data/datasource/local/HoppyHourDatabase.kt
@@ -8,8 +8,18 @@ import com.josphatmwania.hoppyhour.feature_beer.domain.model.BeerEntity
 
 @Database(
     entities = [BeerEntity::class],
-    version = Constants.DATABASE_VERSION
+    version = Constants.DATABASE_VERSION,
+    exportSchema = false
 )
 abstract class HoppyHourDatabase : RoomDatabase() {
     abstract fun dao(): HoppyHourDao
+
+    /**
+     * allows access to the methods to create or
+     * get the database and uses the class name as the qualifier.
+     */
+
+    companion object{
+        private val Instance: HoppyHourDatabase? = null
+    }
 }
